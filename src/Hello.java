908afe1f66cd7c45ba0cdb8a5d9863403a8d36e5
Gleb Ponomarev <36563095+Tonzako@users.noreply.github.com>
2018-12-05 01:29:17 +0300

    Add new method helloPerson.

--- src/Hello.java
+++ src/Hello.java
@@ -1,6 +1,11 @@
 public class Hello {
+
     public static void helloWorld() {
         System.out.println("HelloWorld");
+    }
+
+    public static void helloPerson(String name) {
+        System.out.println("Hello " + name);
     }
 
 }
